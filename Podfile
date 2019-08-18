8c14086e0bff204167850d2c48c50f6e2b164f31
ochimusha <blacktearsdontfall@gmail.com>
2019-08-18 21:11:08 +0900

    地球と宇宙船を表示

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '10.0'
+
+target 'shooting-game' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for shooting-game
+  pod 'SwiftFormat/CLI'
+  pod 'SwiftLint'
+
+end
--- shooting-game/GameScene.swift
+++ shooting-game/GameScene.swift
@@ -10,7 +10,23 @@ import GameplayKit
 import SpriteKit
 
 class GameScene: SKScene {
-    override func didMove(to view: SKView) {
+    var earth: SKSpriteNode!
+    var spaceShip: SKSpriteNode!
 
+    override func didMove(to view: SKView) {
+        self.earth = SKSpriteNode(imageNamed: "earth")
+        self.earth.xScale = 1.5
+        self.earth.yScale = 0.3
+        self.earth.position = CGPoint(x: 0, y: -frame.height / 2)
+        self.earth.zPosition = -1.0
+        addChild(self.earth)
+
+        self.spaceShip = SKSpriteNode(imageNamed: "spaceship")
+        self.spaceShip.scale(to: CGSize(width: frame.width / 5, height: frame.width / 5))
+        self.spaceShip.position = CGPoint(x: 0, y: self.earth.frame.maxY + 50)
+        addChild(self.spaceShip)
+    }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
     }
 }
